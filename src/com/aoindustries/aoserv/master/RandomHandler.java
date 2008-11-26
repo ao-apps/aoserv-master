4bd25346cd6a0bba650be9422cb774ac5794fe60
Dan Armstrong <dan@aoindustries.com>
2008-11-26 18:41:43 +0000

    Removed profiling.

--- src/com/aoindustries/aoserv/master/RandomHandler.java
+++ src/com/aoindustries/aoserv/master/RandomHandler.java
@@ -5,11 +5,14 @@ package com.aoindustries.aoserv.master;
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
-import com.aoindustries.aoserv.client.*;
-import com.aoindustries.io.*;
-import com.aoindustries.profiler.*;
-import java.io.*;
-import java.sql.*;
+import com.aoindustries.aoserv.client.AOServConnector;
+import com.aoindustries.aoserv.client.MasterUser;
+import com.aoindustries.io.FifoFile;
+import com.aoindustries.io.FifoFileInputStream;
+import com.aoindustries.io.FifoFileOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * The <code>RandomHandler</code> stores obtains a pool of random data from servers that have excess and provides
@@ -22,48 +25,38 @@ public final class RandomHandler {
     private static FifoFile fifoFile;
     
     public static FifoFile getFifoFile() throws IOException {
-        Profiler.startProfile(Profiler.FAST, RandomHandler.class, "getFifoFile()", null);
-        try {
-            synchronized(RandomHandler.class) {
-                if(fifoFile==null) fifoFile=new FifoFile(MasterConfiguration.getEntropyPoolFilePath(), AOServConnector.MASTER_ENTROPY_POOL_SIZE);
-                return fifoFile;
-            }
-        } finally {
-            Profiler.endProfile(Profiler.FAST);
+        synchronized(RandomHandler.class) {
+            if(fifoFile==null) fifoFile=new FifoFile(MasterConfiguration.getEntropyPoolFilePath(), AOServConnector.MASTER_ENTROPY_POOL_SIZE);
+            return fifoFile;
         }
     }
     
     private static void checkAccessEntropy(MasterDatabaseConnection conn, RequestSource source, String action) throws IOException, SQLException {
-        Profiler.startProfile(Profiler.FAST, RandomHandler.class, "checkAccessEntropy(MasterDatabaseConnection,RequestSource,String)", null);
-        try {
-            boolean isAllowed=false;
+        boolean isAllowed=false;
 
-            String mustring=source.getUsername();
-            MasterUser mu = MasterServer.getMasterUser(conn, mustring);
-            if (mu!=null) {
-                com.aoindustries.aoserv.client.MasterServer[] masterServers=MasterServer.getMasterServers(conn, mustring);
-                if(masterServers.length==0) isAllowed=true;
-                else {
-                    for(int c=0;c<masterServers.length;c++) {
-                        if(ServerHandler.isAOServer(conn, masterServers[c].getServerPKey())) {
-                            isAllowed=true;
-                            break;
-                        }
+        String mustring=source.getUsername();
+        MasterUser mu = MasterServer.getMasterUser(conn, mustring);
+        if (mu!=null) {
+            com.aoindustries.aoserv.client.MasterServer[] masterServers=MasterServer.getMasterServers(conn, mustring);
+            if(masterServers.length==0) isAllowed=true;
+            else {
+                for(int c=0;c<masterServers.length;c++) {
+                    if(ServerHandler.isAOServer(conn, masterServers[c].getServerPKey())) {
+                        isAllowed=true;
+                        break;
                     }
                 }
             }
-            if(!isAllowed) {
-                String message=
-                    "business_administrator.username="
-                    +mustring
-                    +" is not allowed to access the master entropy pool: action='"
-                    +action
-                ;
-                MasterServer.reportSecurityMessage(source, message);
-                throw new SQLException(message);
-            }
-        } finally {
-            Profiler.endProfile(Profiler.FAST);
+        }
+        if(!isAllowed) {
+            String message=
+                "business_administrator.username="
+                +mustring
+                +" is not allowed to access the master entropy pool: action='"
+                +action
+            ;
+            MasterServer.reportSecurityMessage(source, message);
+            throw new SQLException(message);
         }
     }
 
@@ -72,20 +65,15 @@ public final class RandomHandler {
         RequestSource source,
         byte[] entropy
     ) throws IOException, SQLException {
-        Profiler.startProfile(Profiler.UNKNOWN, RandomHandler.class, "addMasterEntropy(MasterDatabaseConnection,RequestSource,byte[])", null);
-        try {
-            checkAccessEntropy(conn, source, "addMasterEntropy");
-            
-            FifoFile file=getFifoFile();
-            synchronized(file) {
-                FifoFileOutputStream fileOut=file.getOutputStream();
-                long available=fileOut.available();
-                int addCount=entropy.length;
-                if(available<addCount) addCount=(int)available;
-                fileOut.write(entropy, 0, addCount);
-            }
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
+        checkAccessEntropy(conn, source, "addMasterEntropy");
+
+        FifoFile file=getFifoFile();
+        synchronized(file) {
+            FifoFileOutputStream fileOut=file.getOutputStream();
+            long available=fileOut.available();
+            int addCount=entropy.length;
+            if(available<addCount) addCount=(int)available;
+            fileOut.write(entropy, 0, addCount);
         }
     }
 
@@ -94,26 +82,21 @@ public final class RandomHandler {
         RequestSource source,
         int numBytes
     ) throws IOException, SQLException {
-        Profiler.startProfile(Profiler.UNKNOWN, RandomHandler.class, "getMasterEntropy(MasterDatabaseConnection,RequestSource,int)", null);
-        try {
-            checkAccessEntropy(conn, source, "getMasterEntropy");
-            
-            FifoFile file=getFifoFile();
-            synchronized(file) {
-                FifoFileInputStream fileIn=file.getInputStream();
-                long available=fileIn.available();
-                if(available<numBytes) numBytes=(int)available;
-                byte[] buff=new byte[numBytes];
-                int pos=0;
-                while(pos<numBytes) {
-                    int ret=fileIn.read(buff, pos, numBytes-pos);
-                    if(ret==-1) throw new EOFException("Unexpected EOF");
-                    pos+=ret;
-                }
-                return buff;
+        checkAccessEntropy(conn, source, "getMasterEntropy");
+
+        FifoFile file=getFifoFile();
+        synchronized(file) {
+            FifoFileInputStream fileIn=file.getInputStream();
+            long available=fileIn.available();
+            if(available<numBytes) numBytes=(int)available;
+            byte[] buff=new byte[numBytes];
+            int pos=0;
+            while(pos<numBytes) {
+                int ret=fileIn.read(buff, pos, numBytes-pos);
+                if(ret==-1) throw new EOFException("Unexpected EOF");
+                pos+=ret;
             }
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
+            return buff;
         }
     }
 
@@ -121,16 +104,11 @@ public final class RandomHandler {
         MasterDatabaseConnection conn,
         RequestSource source
     ) throws IOException, SQLException {
-        Profiler.startProfile(Profiler.UNKNOWN, RandomHandler.class, "getMasterEntropyNeeded(MasterDatabaseConnection,RequestSource)", null);
-        try {
-            checkAccessEntropy(conn, source, "getMasterEntropyNeeded");
-            
-            FifoFile file=getFifoFile();
-            synchronized(file) {
-                return file.getMaximumFifoLength()-file.getLength();
-            }
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
+        checkAccessEntropy(conn, source, "getMasterEntropyNeeded");
+
+        FifoFile file=getFifoFile();
+        synchronized(file) {
+            return file.getMaximumFifoLength()-file.getLength();
         }
     }
 }
