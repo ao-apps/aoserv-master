f01099ecd7db66576b800ca5812cc27d3978663a
Dan Armstrong <dan@aoindustries.com>
2009-01-06 22:16:37 +0000

    Updated copyrights.

--- src/com/aoindustries/aoserv/master/BackupHandler.java
+++ src/com/aoindustries/aoserv/master/BackupHandler.java
@@ -1,7 +1,7 @@
 package com.aoindustries.aoserv.master;
 
 /*
- * Copyright 2002-2008 by AO Industries, Inc.,
+ * Copyright 2002-2009 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
