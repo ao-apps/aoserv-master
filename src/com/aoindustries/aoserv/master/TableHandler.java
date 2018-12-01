101ea7b9bacb3ca9d776c9fa29ffdec59f486df9
Dan Armstrong <dan@aoindustries.com>
2018-12-01 11:01:35 +0000

    Working on making getTable more modular.  A 10,000 line class is a bit of a problem.

--- src/com/aoindustries/aoserv/master/TableHandler.java
+++ src/com/aoindustries/aoserv/master/TableHandler.java
@@ -17,10 +17,6 @@ import com.aoindustries.aoserv.client.master.Permission;
 import com.aoindustries.aoserv.client.master.User;
 import com.aoindustries.aoserv.client.master.UserHost;
 import com.aoindustries.aoserv.client.net.Host;
-import com.aoindustries.aoserv.client.reseller.Brand;
-import com.aoindustries.aoserv.client.reseller.BrandCategory;
-import com.aoindustries.aoserv.client.reseller.Category;
-import com.aoindustries.aoserv.client.reseller.Reseller;
 import com.aoindustries.aoserv.client.schema.AoservProtocol;
 import com.aoindustries.aoserv.client.schema.Column;
 import com.aoindustries.aoserv.client.schema.ForeignKey;
@@ -743,101 +739,6 @@ final public class TableHandler {
 						new AoservProtocol(),
 						"select * from \"schema\".\"AoservProtocol\""
 					);
-					break;
-				case BRANDS :
-					if(masterUser != null) {
-						assert masterServers != null;
-						if(masterServers.length == 0) {
-							MasterServer.writeObjects(
-								conn,
-								source,
-								out,
-								provideProgress,
-								new Brand(),
-								"select * from reseller.\"Brand\""
-							);
-						} else {
-							// Limited access to its own brand only
-							MasterServer.writeObjects(
-								conn,
-								source,
-								out,
-								provideProgress,
-								new Brand(),
-								"select\n"
-								+ "  br.accounting,\n"
-								+ "  br.nameserver1,\n"
-								+ "  br.nameserver2,\n"
-								+ "  br.nameserver3,\n"
-								+ "  br.nameserver4,\n"
-								+ "  br.smtp_linux_server_account,\n"
-								+ "  null::text,\n" // smtp_host
-								+ "  '"+AoservProtocol.FILTERED+"'::text,\n" // smtp_password
-								+ "  br.imap_linux_server_account,\n"
-								+ "  null::text,\n" // imap_host
-								+ "  '"+AoservProtocol.FILTERED+"'::text,\n" // imap_password
-								+ "  br.support_email_address,\n"
-								+ "  br.support_email_display,\n"
-								+ "  br.signup_email_address,\n"
-								+ "  br.signup_email_display,\n"
-								+ "  br.ticket_encryption_from,\n"
-								+ "  br.ticket_encryption_recipient,\n"
-								+ "  br.signup_encryption_from,\n"
-								+ "  br.signup_encryption_recipient,\n"
-								+ "  br.support_toll_free,\n"
-								+ "  br.support_day_phone,\n"
-								+ "  br.support_emergency_phone1,\n"
-								+ "  br.support_emergency_phone2,\n"
-								+ "  br.support_fax,\n"
-								+ "  br.support_mailing_address1,\n"
-								+ "  br.support_mailing_address2,\n"
-								+ "  br.support_mailing_address3,\n"
-								+ "  br.support_mailing_address4,\n"
-								+ "  br.english_enabled,\n"
-								+ "  br.japanese_enabled,\n"
-								+ "  br.aoweb_struts_http_url_base,\n"
-								+ "  br.aoweb_struts_https_url_base,\n"
-								+ "  br.aoweb_struts_google_verify_content,\n"
-								+ "  br.aoweb_struts_noindex,\n"
-								+ "  br.aoweb_struts_google_analytics_new_tracking_code,\n"
-								+ "  br.aoweb_struts_signup_admin_address,\n"
-								+ "  br.aoweb_struts_vnc_bind,\n"
-								+ "  '"+AoservProtocol.FILTERED+"'::text,\n" // aoweb_struts_keystore_type
-								+ "  '"+AoservProtocol.FILTERED+"'::text\n" // aoweb_struts_keystore_password
-								+ "from\n"
-								+ "  account.\"Username\" un,\n"
-								+ "  billing.\"Package\" pk,\n"
-								+ "  reseller.\"Brand\" br\n"
-								+ "where\n"
-								+ "  un.username=?\n"
-								+ "  and un.package=pk.name\n"
-								+ "  and pk.accounting=br.accounting",
-								username
-							);
-						}
-					} else {
-						MasterServer.writeObjects(
-							conn,
-							source,
-							out,
-							provideProgress,
-							new Brand(),
-							"select\n"
-							+ "  br.*\n"
-							+ "from\n"
-							+ "  account.\"Username\" un,\n"
-							+ "  billing.\"Package\" pk,\n"
-							+ TableHandler.BU1_PARENTS_JOIN
-							+ "  reseller.\"Brand\" br\n"
-							+ "where\n"
-							+ "  un.username=?\n"
-							+ "  and un.package=pk.name\n"
-							+ "  and (\n"
-							+ TableHandler.PK_BU1_PARENTS_WHERE
-							+ "  ) and bu1.accounting=br.accounting",
-							username
-						);
-					}
 					break;
 				case CVS_REPOSITORIES :
 					if(masterUser != null) {
@@ -2079,41 +1980,6 @@ final public class TableHandler {
 						provideProgress
 					);
 					break;
-				case RESELLERS :
-					if(masterUser != null) {
-						assert masterServers != null;
-						if(masterServers.length == 0) MasterServer.writeObjects(
-							conn,
-							source,
-							out,
-							provideProgress,
-							new Reseller(),
-							"select * from reseller.\"Reseller\""
-						); else {
-							MasterServer.writeObjects(source, out, provideProgress, Collections.emptyList());
-						}
-					} else MasterServer.writeObjects(
-						conn,
-						source,
-						out,
-						provideProgress,
-						new Reseller(),
-						"select\n"
-						+ "  re.*\n"
-						+ "from\n"
-						+ "  account.\"Username\" un,\n"
-						+ "  billing.\"Package\" pk,\n"
-						+ TableHandler.BU1_PARENTS_JOIN
-						+ "  reseller.\"Reseller\" re\n"
-						+ "where\n"
-						+ "  un.username=?\n"
-						+ "  and un.package=pk.name\n"
-						+ "  and (\n"
-						+ TableHandler.PK_BU1_PARENTS_WHERE
-						+ "  ) and bu1.accounting=re.accounting",
-						username
-					);
-					break;
 				case SCHEMA_COLUMNS :
 					{
 						List<Column> clientColumns=new ArrayList<>();
@@ -2672,51 +2538,6 @@ final public class TableHandler {
 							MasterServer.writeObjects(source, out, provideProgress, Collections.emptyList());
 						}
 					}
-					break;
-				case TICKET_BRAND_CATEGORIES :
-					if(masterUser != null) {
-						assert masterServers != null;
-						if(masterServers.length == 0) MasterServer.writeObjects(
-							conn,
-							source,
-							out,
-							provideProgress,
-							new BrandCategory(),
-							"select * from reseller.\"BrandCategory\""
-						); else {
-							MasterServer.writeObjects(source, out, provideProgress, Collections.emptyList());
-						}
-					} else MasterServer.writeObjects(
-						conn,
-						source,
-						out,
-						provideProgress,
-						new BrandCategory(),
-						"select\n"
-						+ "  tbc.*\n"
-						+ "from\n"
-						+ "  account.\"Username\" un,\n"
-						+ "  billing.\"Package\" pk,\n"
-						+ TableHandler.BU1_PARENTS_JOIN
-						+ "  reseller.\"BrandCategory\" tbc\n"
-						+ "where\n"
-						+ "  un.username=?\n"
-						+ "  and un.package=pk.name\n"
-						+ "  and (\n"
-						+ TableHandler.PK_BU1_PARENTS_WHERE
-						+ "  ) and bu1.accounting=tbc.brand",
-						username
-					);
-					break;
-				case TICKET_CATEGORIES :
-					MasterServer.writeObjects(
-						conn,
-						source,
-						out,
-						provideProgress,
-						new Category(),
-						"select * from reseller.\"Category\""
-					);
 					break;
 				case TICKET_PRIORITIES :
 					MasterServer.writeObjects(
