2b470d4b8d49a288d324408b3ebe581652b8962f
AO Industries, Inc <support@aoindustries.com>
2023-09-07 08:26:43 -0500

    Fixed incorrectly inverted log levels polluting log files

--- src/main/java/com/aoindustries/aoserv/master/SocketServerThread.java
+++ src/main/java/com/aoindustries/aoserv/master/SocketServerThread.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-master - Master server for the AOServ Platform.
- * Copyright (C) 2001-2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2001-2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -210,9 +210,9 @@ public final class SocketServerThread extends Thread implements RequestSource {
                       || "Remote host closed connection during handshake".equals(message)
               )
           ) {
-            level = Level.FINE;
-          } else {
             level = Level.SEVERE;
+          } else {
+            level = Level.FINE;
           }
           logger.log(level, null, err);
           return;
