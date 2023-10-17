ec3eecf95a6c9d291e9ea95d4b198f931af3f04e
AO Industries, Inc <support@aoindustries.com>
2023-10-17 12:05:52 -0500

    Allow nullable and optional

--- src/main/java/com/aoindustries/aoserv/master/FailoverHandler.java
+++ src/main/java/com/aoindustries/aoserv/master/FailoverHandler.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-master - Master server for the AOServ Platform.
- * Copyright (C) 2001-2013, 2015, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2001-2013, 2015, 2017, 2018, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -450,7 +450,7 @@ public final class FailoverHandler implements CronJob {
       }
     }
 
-    HostAddress connectAddress = conn.queryObject(
+    HostAddress connectAddress = conn.queryObjectNullable(
         ObjectFactories.hostAddressFactory,
         "select connect_address from backup.\"FileReplication\" where id=?",
         fileReplication
